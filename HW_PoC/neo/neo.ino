7aea5b2e62dcfaa7ed189fe79a9962fa622942bd
Donghyun Kim <inerplat@gmail.com>
2017-10-19 13:56:37 +0900

    NeoPixel example

---
+++ HW_PoC/neo/neo.ino
@@ -0,0 +1,108 @@
+
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(100, PIN, NEO_GRB + NEO_KHZ800);
+ 
+
+void setup() {
+  strip.begin();
+  strip.show(); 
+}
+
+void loop() {
+//  colorWipe(strip.Color(255, 255, 255), 100); //빨간색 출력
+ // colorWipe(strip.Color(0, 255, 0), 0); //녹색 출력
+ // colorWipe(strip.Color(0, 0, 255), 0); //파란색 출력
+
+//  theaterChase(strip.Color(127, 127, 127), 50); //흰색 출력
+//  theaterChase(strip.Color(127,   0,   0), 50); //빨간색 출력
+//  theaterChase(strip.Color(  0,   0, 127), 50); //파란색 출력
+
+//  rainbow(20);
+//  rainbowCycle(20);
+  theaterChaseRainbow(50);
+  delay(100);
+  colorWipe(strip.Color(0, 0, 0), 0);
+  delay(100);
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { 
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+        delay(100);
+      }
+      strip.show();
+     
+      delay(wait);
+     
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     //256가지의 색을 표현
+    for (int q=0; q < 3; q++) {
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, Wheel( (i+j) % 255));
+        }
+        strip.show();
+       
+        delay(wait);
+       
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, 0); 
+        }
+    }
+  }
+}
+
+//255가지의 색을 나타내는 함수
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
