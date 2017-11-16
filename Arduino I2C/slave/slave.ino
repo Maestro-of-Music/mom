c24ad4114727419aec11af5ea85c4a3205a38d5a
DH Kim <inerplat@gmail.com>
2017-11-17 05:20:58 +0900

    Update slave.ino
    testing record and send2master function
    it should change to control led and to read sensor data

--- Arduino I2C/slave/slave.ino
+++ Arduino I2C/slave/slave.ino
@@ -1,31 +1,40 @@
 #include <Wire.h>
- 
+
 #define Slave1 1 // Slave address
 char rec[128]; // buffer
- 
+char hi;
 void setup() {
-   Wire.begin(Slave1); // init to i2c-slave
+   Wire.begin(Slave1); // init to i2c-slavek
    Wire.onRequest(sendToMaster); // if master request data -> then excute sendToMaster func
    Serial.begin(9600);
+   pinMode(2,INPUT_PULLUP);
+   pinMode(3,INPUT_PULLUP);
 }
- 
+int stack[101];
+int top=0;
 void loop () {
-   Wire.onReceive(record); // if slave receve data -> then excute record func
-   if(rec[0] == 1)
-   {
-     Serial.println("Slave1 : Hi!");
-     rec[0]=0;
-     delay(1);
-   }
+  Wire.onReceive(record);
+  if(!digitalRead(2))
+  {
+    stack[++top]=2;
+  }
+  if(!digitalRead(3))
+  {
+    stack[++top]=3;
+  }
 }
  
 void record(int receiveNum){
-   for(int i = 0; i < receiveNum; i++){
-     rec[i] = Wire.read();
-   }
+
+  // int tmp=int(Wire.read());
+  char c=Wire.read();
+   Serial.println(c);
 }
 
 void sendToMaster() {
-   Wire.write(65);
+  if(top)
+  {
+   Wire.write(stack[top--]);
+  }
 }
  
