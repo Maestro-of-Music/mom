8175e24600e90a2633b99a111cddd886e9cc8c39
DH Kim <inerplat@gmail.com>
2017-11-25 11:34:02 +0900

    Update master.ino

--- Arduino I2C/master/master.ino
+++ Arduino I2C/master/master.ino
@@ -11,24 +11,26 @@ SoftwareSerial mySerial(blueTx, blueRx);
 void setup() {
    Wire.begin(); // Init to i2c-master
    Serial.begin(9600); // Init to serial
-   mySerial.begin(9600);
+   mySerial.begin(115200);
 }
  
 void loop() {
 
 /////////////////Android->Master->Slave///////////////////
 //
+
   if(mySerial.available())
   {
     delay(1);
     char data=mySerial.read();
+    data-='!';
     int device=data/12+1;
     Serial.print("data :");
-    Serial.print(data);
+    Serial.print(int(data));
     Serial.print(" device : ");
     Serial.println(device);
     Wire.beginTransmission(device);
-    Wire.write((data-1)%12+'A');
+    Wire.write((data-1)%12+'A'+1);
     Wire.endTransmission();
   }
 //
@@ -39,14 +41,14 @@ void loop() {
  for(int i=1;i<=3;i++)
    {
        Wire.requestFrom(i, 1); // Request 1byte data to slave1
-       if(Wire.available())
-       {
+       delay(3);
+       
         char s = Wire.read(); // Read data
-        if(s>=1){
-        Serial.println(int((i-1)*12+s));
-        mySerial.write((i-1)*12+s);
+        if((s>='A' && s<='Z') || (s>='a' && s<='z')){
+        Serial.println(s);
+        mySerial.write((i-1)*12+(s-'A')+33);
         }
-       }
+       
        else break;
    }
 //
